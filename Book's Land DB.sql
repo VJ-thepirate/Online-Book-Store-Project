d46f32aedcb35505a27e3f0aa658826f06cdd8e5
VJ-thepirate <96567137+VJ-thepirate@users.noreply.github.com>
2022-03-05 12:21:21 +0530

    Add files via upload

---
+++ Book's Land DB.sql
@@ -0,0 +1,137 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: booksland
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_details`
+--
+
+DROP TABLE IF EXISTS `admin_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_details` (
+  `user_name` varchar(15) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `emp_Id` varchar(15) NOT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_details`
+--
+
+LOCK TABLES `admin_details` WRITE;
+/*!40000 ALTER TABLE `admin_details` DISABLE KEYS */;
+INSERT INTO `admin_details` VALUES ('vj','123','vijayaraj','001');
+/*!40000 ALTER TABLE `admin_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `Book_Code` varchar(10) NOT NULL,
+  `Book_Name` varchar(50) NOT NULL,
+  `Author` varchar(30) NOT NULL,
+  `pages` int NOT NULL,
+  `price` double NOT NULL,
+  `stock` int NOT NULL,
+  UNIQUE KEY `Book_Code` (`Book_Code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES ('422','C++','Bjarne Stroustrup',1250,500,7),('511','java','james',1100,600,6),('611','Python','Guido van Rossum',600,700,15),('720','HTML','Tim Berners-Lee',400,120,22),('813','Java Script','Brendan Eich',150,650,8);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_details`
+--
+
+DROP TABLE IF EXISTS `order_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_details` (
+  `Book_Code` varchar(10) NOT NULL,
+  `Book_Name` varchar(50) NOT NULL,
+  `Quantity` int NOT NULL,
+  `delivery_address` varchar(50) NOT NULL,
+  UNIQUE KEY `Book_Code` (`Book_Code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_details`
+--
+
+LOCK TABLES `order_details` WRITE;
+/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_details`
+--
+
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_details` (
+  `user_name` varchar(15) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `landmark` varchar(50) NOT NULL,
+  `mobile` bigint NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_name`),
+  UNIQUE KEY `mobile` (`mobile`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_details`
+--
+
+LOCK TABLES `user_details` WRITE;
+/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+INSERT INTO `user_details` VALUES ('abhishek','123','Abhishek_Kumar','No:8,4th_street,chrompet,chennai','Yeses_Super_Market',9790790907,'abhishek@gmail.com'),('jai','123','jayakumar','chennai','annanagar',8939362847,'vivajaya@gmail.com'),('vijay','123','vijayaraj','No:3/7,AnnaNagar,Chennai-102','chinthamani',7845955148,'jazz007.vr@gmail.com');
+/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-05 12:19:00
